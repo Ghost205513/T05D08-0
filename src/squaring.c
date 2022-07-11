52989add91e3b8581a080848cd21e3651a0f139c
lucioana <nikita.suslov.03@mail.ru>
2022-07-11 18:14:18 +0300

    quest 2

--- src/squaring.c
+++ src/squaring.c
@@ -1,37 +1,57 @@
 #include <stdio.h>
 #define NMAX 10
 
+
 int input(int *a, int *n);
 void output(int *a, int n);
 void squaring(int *a, int n);
 
-int main()
-{
+int main() {
     int n, data[NMAX];
-    input(data, n);
+
+    if (input(data, &n) == 1) {
+        printf("n/a");
+        return 1;
+    }
+
     squaring(data, n);
     output(data, n);
 
     return 0;
 }
 
-int input(int *a, int *n)
-{
-    scanf("%d", n);
-    for(int *p = a; p - a < *n; p++)
-    {
-        scanf("%d", p);
+int input(int a[], int *n) {
+    if (scanf("%d", n) == 0)
+        return 1;
+
+    for (int *p = a; p - a < *n; p++)
+        if (scanf("%d", p) == 0)
+            return 1;
+    char trash = getchar();
+
+    while (1) {
+        if (trash != ' ' && trash != '\t')
+            break;
+
+        trash = getchar();
     }
+
+    if (trash != '\n')
+        return 1;
 }
 
-void output(int *a, int n)
-{
-    //NOTHING
+void output(int a[], int n) {
+    printf("%d", *a);
+
+    for (int i = 1; i < n; i++)
+        printf(" %d", *(a + i));
+
+    printf("\n");
 }
 
-void squaring(int *a, int n)
-{
-    //NOTHING
+void squaring(int a[], int n) {
+    for (int i = 0; i < n; i++)
+        *(a + i) = a[i] * a[i];
 }
 
 
